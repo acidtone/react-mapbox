70306d05a2f87ef0878d069168c5c14953f438c4
Tony Grimes <acidtone@tonygrimes.com>
2023-04-29 17:34:30 -0600

    added popup (broken)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Map, {Marker} from 'react-map-gl'
+import Map, {Marker, Popup} from 'react-map-gl'
 import './App.css'
 import {policeData} from './data/police-service'
 import './mapbox-gl.css'
@@ -10,22 +10,43 @@ function App() {
     longitude: '-114.070390',
     zoom: 10,
   })
+  const [selectedStation, setSelectedStation] = useState(null);
+  console.log(selectedStation);
+
   return (
+    <div>
+
       <Map 
         {...viewport} 
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         style={{width: '100vw', height: '100vh'}}
         onMove={evt => setViewport(evt.viewState)}>
-        {policeData.features.map(item => (
+        {policeData.features.map(station => (
           <Marker
-            key={item.properties.name}
-            latitude={item.geometry.coordinates[1]}
-            longitude={item.geometry.coordinates[0]}>
-            <button className="marker-btn"><img src="/police.svg" alt="Police Station" /></button>
+            key={station.properties.name}
+            latitude={station.geometry.coordinates[1]}
+            longitude={station.geometry.coordinates[0]}>
+            <button 
+              className="marker-btn"
+              onClick={(evt) => {
+                evt.preventDefault();
+                setSelectedStation(station);
+              }}><img src="/police.svg" alt="Police Station" /></button>
           </Marker>
         ))}
+        {selectedStation ? (
+          <Popup latitude={selectedStation.geometry.coordinates[1]} longitude={selectedStation.geometry.coordinates[0]}>
+            <div>
+              <h2>{selectedStation.properties.name}</h2>
+              <p>{selectedStation.properties.info}<br/>
+                {selectedStation.properties.address}<br/>
+                {selectedStation.properties.station_ty}</p>
+            </div>
+          </Popup>
+        ) : null}
       </Map>
+    </div>
   )
 }
 
